9188ebaa375b293c130ef7951d20815912690c2a
Sarvar <ksz95@bk.ru>
2024-07-07 01:48:32 +0500

    feat: add home page

--- class-components/.eslintrc.cjs
+++ class-components/.eslintrc.cjs
@@ -1,28 +1,27 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
     'plugin:react/recommended',
-    'plugin:react-compiler/recommended',
+    'plugin:react-hooks/recommended',
     'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', '@typescript-eslint', 'prettier', 'react-compiler'],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
     '@typescript-eslint/no-explicit-any': 'error',
     'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
   },
   settings: {
     react: {
       version: 'detect',
     },
   },
-}
+};
---
+++ class-components/src/pages/HomePage.tsx
@@ -0,0 +1,61 @@
+import { Component, ChangeEvent } from 'react';
+import { fetchItems } from '../api';
+import SearchInput from '../components/Search/SearchInput';
+import SearchButton from '../components/Search/SearchButton';
+import SearchResults from '../components/SearchResults/SearchResults';
+
+interface State {
+  searchTerm: string;
+  results: any[];
+}
+
+class HomePage extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    const savedSearchTerm = localStorage.getItem('searchTerm') || '';
+    this.state = {
+      searchTerm: savedSearchTerm,
+      results: [],
+    };
+  }
+
+  componentDidMount() {
+    this.performSearch();
+  }
+
+  handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  handleSearchClick = () => {
+    this.performSearch();
+  };
+
+  performSearch = async () => {
+    const { searchTerm } = this.state;
+    try {
+      const data = await fetchItems(searchTerm.trim());
+      this.setState({ results: data.results });
+      localStorage.setItem('searchTerm', searchTerm.trim());
+    } catch (error) {
+      console.error('Search failed', error);
+    }
+  };
+
+  render() {
+    const { searchTerm, results } = this.state;
+    return (
+      <div>
+        <div>
+          <SearchInput value={searchTerm} onChange={this.handleInputChange} />
+          <SearchButton onClick={this.handleSearchClick} />
+        </div>
+        <div>
+          <SearchResults results={results} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
