579fb4aa9aed7c9669867215b435b3f1d2a3225c
Sarvar <ksz95@bk.ru>
2024-07-10 03:46:20 +0500

    refactor: refactor prettier

--- class-components/.eslintrc.cjs
+++ class-components/.eslintrc.cjs
@@ -13,12 +13,15 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react', '@typescript-eslint', 'prettier', "react-compiler"],
+  plugins: ['react', '@typescript-eslint', 'prettier', 'react-compiler'],
   rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
     '@typescript-eslint/no-explicit-any': 'error',
     'prettier/prettier': 'error',
-    'react/react-in-jsx-scope': 'off',
-    "react-compiler/react-compiler": "error"
+    'react-compiler/react-compiler': 'error',
   },
   settings: {
     react: {
