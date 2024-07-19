389e52254b78d9fb71a90df65ad52cc1abaa7ba3
Sarvar <ksz95@bk.ru>
2024-07-20 00:33:23 +0500

    feat: add RTK

--- class-components/src/api/index.ts
+++ class-components/src/api/index.ts
@@ -1,4 +1,7 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import type { SearchResult, CharacterCard } from '../types/SearchResults';
+import { url } from 'inspector';
+
 const API_BASE_URL = 'https://swapi.dev/api/people/';
 
 export async function searchItems(
@@ -27,3 +30,25 @@ export async function searchItem(
     console.error(error);
   }
 }
+
+export const searchApi = createApi({
+  reducerPath: 'searchApi',
+  baseQuery: fetchBaseQuery({ baseUrl: API_BASE_URL }),
+  endpoints: (builder) => ({
+    searchItems: builder.query<
+      SearchResult,
+      { searchTerm: string; page: number }
+    >({
+      query: ({ searchTerm, page }) => ({
+        url: `?search=${searchTerm}&page=${page}`,
+      }),
+    }),
+    searchItem: builder.query<CharacterCard, string>({
+      query: (id) => ({
+        url: `${id}`,
+      }),
+    }),
+  }),
+});
+
+export const { useSearchItemsQuery, useSearchItemQuery } = searchApi;
